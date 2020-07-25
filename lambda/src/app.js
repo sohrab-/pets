b2e17978bb6bc729de89f5a300966cba5ab468b2
Chen, Sandra (AU - Melbourne) <sandchen@deloitte.com.au>
2020-07-26 02:10:39 +1000

    feat: add rekognition and s3 interactions to lambda

--- lambda/src/app.js
+++ lambda/src/app.js
@@ -1,5 +1,9 @@
-const { DynamoDB } = require("aws-sdk");
+const { DynamoDB, Rekognition, S3 } = require("aws-sdk");
 const { v4: uuidv4 } = require("uuid");
+
+const db = new DynamoDB({ apiVersion: "2012-08-10" });
+const s3 = new S3();
+const rekognition = new Rekognition();
 
 const supportedPets = [
   "cat",
@@ -14,6 +18,7 @@ const supportedPets = [
 ];
 
 const tableName = "pets";
+const bucketName = "pets-soe-dpe-au-images";
 
 // DynamoDB doesn't like null values
 const removeNullFields = (item) => {
@@ -43,63 +48,52 @@ const removeNullFields = (item) => {
  */
 exports.lambdaHandler = async (event, context) => {
   try {
-    // TODO remove
-    console.log(JSON.stringify(event));
-    console.log(JSON.stringify(context));
-
     const body = JSON.parse(event.body);
-    let pet = body.pet;
-    if (event.body.image) {
-      // TODO invoke rekognition
-      // TODO set 'pet' to returned value
+    let type = body.type;
+    let imageBuffer = null;
+
+    if (body.image) {
+      imageBuffer = getImageBuffer(body.image);
+
+      const results = await rekognizeLabels(imageBuffer);
+      const supportedLabels = results.Labels.map(
+        (label) => label.Name
+      ).filter((name) => supportedPets.includes(name.toLowerCase()));
+
+      // Get matching supported pet label if available
+      if (supportedLabels.length > 0) {
+        type = supportedLabels[0].toLowerCase();
+      } else {
+        // Get first label it has been identified as
+        type = results.Labels[0].Name.toLowerCase();
+      }
     }
 
-    console.log(pet);
-    if (supportedPets.indexOf(pet) > -1) {
+    if (supportedPets.indexOf(type) > -1) {
       let id = uuidv4();
-      let imagePath = null;
-      if (event.body.image) {
-        // TODO store in S3 as '<id>.<ext>'
-        // TODO set 'imagePath' to S3 URL
+      let imageFileName = null;
+
+      if (body.image) {
+        const moderationResult = await rekognizeModerationLabels(imageBuffer);
+
+        // Only upload image if it does not have unsafe content
+        if (moderationResult.ModerationLabels.length === 0) {
+          imageFileName = id + "." + getFileType(body.image);
+          await uploadImageToS3(imageFileName, imageBuffer);
+        }
       }
 
-      const item = {
-        id: { S: id },
-        pet: { S: pet },
-        imagePath: { S: imagePath },
-        userAgent: { S: event.headers["User-Agent"] },
-        desktopClient: {
-          BOOL: event.headers["CloudFront-Is-Desktop-Viewer"] === true,
-        },
-        mobileClient: {
-          BOOL: event.headers["CloudFront-Is-Mobile-Viewer"] === true,
-        },
-        smartTvClient: {
-          BOOL: event.headers["CloudFront-Is-SmartTV-Viewer"] === true,
-        },
-        tabletClient: {
-          BOOL: event.headers["CloudFront-Is-Tablet-Viewer"] === true,
-        },
-        country: { S: event.headers["CloudFront-Viewer-Country"] },
-        // TODO any other useful info that can be gleaned?
-      };
-
-      removeNullFields(item);
-      console.log(item);
-
-      // insert into DynamoDB
-      const db = new DynamoDB({ apiVersion: "2012-08-10" });
-      await db.putItem({ TableName: tableName, Item: item }).promise();
+      await insertIntoDynamo(event, id, type, imageFileName);
 
       return {
         statusCode: 201,
-        body: JSON.stringify({ pet }),
+        body: JSON.stringify({ type }),
       };
     } else {
       // bad request
       return {
         statusCode: 400,
-        body: JSON.stringify({ message: `${pet} is not a supported pet` }),
+        body: JSON.stringify({ message: `${type} is not a supported pet` }),
       };
     }
   } catch (err) {
@@ -110,4 +104,72 @@ exports.lambdaHandler = async (event, context) => {
       body: JSON.stringify({ message: "Internal Server Error" }),
     };
   }
+};
+
+const rekognizeLabels = async (imageBytes) => {
+  const params = {
+    Image: {
+      Bytes: imageBytes,
+    },
+    MaxLabels: 10,
+  };
+
+  return rekognition.detectLabels(params).promise();
+};
+
+const rekognizeModerationLabels = async (imageBytes) => {
+  const params = {
+    Image: {
+      Bytes: imageBytes,
+    },
+  };
+
+  return rekognition.detectModerationLabels(params).promise();
+};
+
+const uploadImageToS3 = async (key, buffer) => {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    Body: buffer,
+    ContentEncoding: "base64",
+    ContentType: "image",
+  };
+  return s3.putObject(params).promise();
+};
+
+const insertIntoDynamo = async (event, id, type, imageFilename) => {
+  const item = {
+    id: { S: id },
+    type: { S: type },
+    imageFilename: { S: imageFilename },
+    userAgent: { S: event.headers["User-Agent"] },
+    desktopClient: {
+      BOOL: event.headers["CloudFront-Is-Desktop-Viewer"] === "true",
+    },
+    mobileClient: {
+      BOOL: event.headers["CloudFront-Is-Mobile-Viewer"] === "true",
+    },
+    smartTvClient: {
+      BOOL: event.headers["CloudFront-Is-SmartTV-Viewer"] === "true",
+    },
+    tabletClient: {
+      BOOL: event.headers["CloudFront-Is-Tablet-Viewer"] === "true",
+    },
+    country: { S: event.headers["CloudFront-Viewer-Country"] },
+    // TODO any other useful info that can be gleaned?
+  };
+
+  removeNullFields(item);
+
+  return db.putItem({ TableName: tableName, Item: item }).promise();
+};
+
+const getFileType = (encodedFile) => {
+  return encodedFile.split("data:image/").pop().split(";base64,")[0];
+};
+
+const getImageBuffer = (encodedFile) => {
+  const base64Image = encodedFile.split("base64,")[1];
+  return Buffer.from(base64Image, "base64");
 };

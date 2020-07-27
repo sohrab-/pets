2b5fbf3340033685d2d125bbaceb6b7e49900a44
Sohrab Hosseini <sohrab.hosseini@gmail.com>
2020-07-28 09:09:30 +1000

    fix(lambda): lookup config from env vars, pass session to DB, move function declarations before they are invoked

--- lambda/src/app.js
+++ lambda/src/app.js
@@ -17,94 +17,8 @@ const supportedPets = [
   "turtle",
 ];
 
-const tableName = "pets";
-const bucketName = "pets-soe-dpe-au-images";
-
-// DynamoDB doesn't like null values
-const removeNullFields = (item) => {
-  ["S", "BOOL"].forEach((type) => {
-    for (let key in item) {
-      if (
-        type in item[key] &&
-        (item[key][type] === null || item[key][type] === undefined)
-      ) {
-        delete item[key];
-      }
-    }
-  });
-};
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-exports.lambdaHandler = async (event, context) => {
-  try {
-    const body = JSON.parse(event.body);
-    let type = body.type;
-    let imageBuffer = null;
-
-    if (body.image) {
-      imageBuffer = getImageBuffer(body.image);
-
-      const results = await rekognizeLabels(imageBuffer);
-      const supportedLabels = results.Labels.map(
-        (label) => label.Name
-      ).filter((name) => supportedPets.includes(name.toLowerCase()));
-
-      // Get matching supported pet label if available
-      if (supportedLabels.length > 0) {
-        type = supportedLabels[0].toLowerCase();
-      } else {
-        // Get first label it has been identified as
-        type = results.Labels[0].Name.toLowerCase();
-      }
-    }
-
-    if (supportedPets.indexOf(type) > -1) {
-      let id = uuidv4();
-      let imageFileName = null;
-
-      if (body.image) {
-        const moderationResult = await rekognizeModerationLabels(imageBuffer);
-
-        // Only upload image if it does not have unsafe content
-        if (moderationResult.ModerationLabels.length === 0) {
-          imageFileName = id + "." + getFileType(body.image);
-          await uploadImageToS3(imageFileName, imageBuffer);
-        }
-      }
-
-      await insertIntoDynamo(event, id, type, imageFileName);
-
-      return {
-        statusCode: 201,
-        body: JSON.stringify({ type }),
-      };
-    } else {
-      // bad request
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: `${type} is not a supported pet` }),
-      };
-    }
-  } catch (err) {
-    // something is terribly wrong
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-    };
-  }
-};
+const tableName = process.env.DB_TABLE;
+const bucketName = process.env.IMAGE_BUCKET;
 
 const rekognizeLabels = async (imageBytes) => {
   const params = {
@@ -142,6 +56,7 @@ const insertIntoDynamo = async (event, id, type, imageFilename) => {
   const item = {
     id: { S: id },
     type: { S: type },
+    demoSession: { S: event.headers["X-Demo-Session"] },
     imageFilename: { S: imageFilename },
     userAgent: { S: event.headers["User-Agent"] },
     desktopClient: {
@@ -160,7 +75,17 @@ const insertIntoDynamo = async (event, id, type, imageFilename) => {
     // TODO any other useful info that can be gleaned?
   };
 
-  removeNullFields(item);
+  // DynamoDB doesn't like null values
+  ["S", "BOOL"].forEach((type) => {
+    for (let key in item) {
+      if (
+        type in item[key] &&
+        (item[key][type] === null || item[key][type] === undefined)
+      ) {
+        delete item[key];
+      }
+    }
+  });
 
   return db.putItem({ TableName: tableName, Item: item }).promise();
 };
@@ -172,4 +97,75 @@ const getFileType = (encodedFile) => {
 const getImageBuffer = (encodedFile) => {
   const base64Image = encodedFile.split("base64,")[1];
   return Buffer.from(base64Image, "base64");
+};
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+exports.lambdaHandler = async (event, context) => {
+  try {
+    const body = JSON.parse(event.body);
+    let type = body.type;
+    let imageBuffer = null;
+
+    if (body.image) {
+      imageBuffer = getImageBuffer(body.image);
+
+      const results = await rekognizeLabels(imageBuffer);
+      const supportedLabels = results.Labels.map(
+        (label) => label.Name
+      ).filter((name) => supportedPets.includes(name.toLowerCase()));
+
+      // Get matching supported pet label if available
+      type =
+        supportedLabels.length > 0
+          ? supportedLabels[0].toLowerCase()
+          : // Get first label it has been identified as
+            results.Labels[0].Name.toLowerCase();
+    }
+
+    if (supportedPets.indexOf(type) > -1) {
+      let id = uuidv4();
+      let imageFileName = null;
+
+      if (body.image) {
+        const moderationResult = await rekognizeModerationLabels(imageBuffer);
+
+        // Only upload image if it does not have unsafe content
+        if (moderationResult.ModerationLabels.length === 0) {
+          imageFileName = id + "." + getFileType(body.image);
+          await uploadImageToS3(imageFileName, imageBuffer);
+        }
+      }
+
+      await insertIntoDynamo(event, id, type, imageFileName);
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify({ type }),
+      };
+    } else {
+      // bad request
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: `${type} is not a supported pet` }),
+      };
+    }
+  } catch (err) {
+    // something is terribly wrong
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
 };

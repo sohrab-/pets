e1bd1c62cd0fbd95397eb8b307f7bae687faa3ff
Sohrab <sohrab.hosseini@gmail.com>
2020-08-02 04:41:23 +1000

    feat: add info to About page (#47)

--- ui/src/components/TopBar.js
+++ ui/src/components/TopBar.js
@@ -6,15 +6,15 @@ import peImage from "../assets/pe.png";
 function TopBar() {
   return (
     <Flex px={2} py={2} color="white" bg="black" sx={{ alignItems: "center" }}>
-      <NavLink px={2} variant="nav" to="/" as={Link}>
+      <NavLink px={2} to="/" as={Link}>
         <img style={{ verticalAlign: "text-bottom" }} src={peImage} alt="Pe." />
         &nbsp;ts
       </NavLink>
       <Box mx="auto" />
-      <NavLink px={2} variant="nav" to="/about" as={Link}>
+      <NavLink px={2} to="/about" as={Link}>
         About
       </NavLink>
-      <NavLink px={2} variant="nav" to="/results" as={Link}>
+      <NavLink px={2} to="/results" as={Link}>
         Results
       </NavLink>
     </Flex>
--- ui/src/pages/About.js
+++ ui/src/pages/About.js
@@ -1,32 +1,76 @@
 import React from "react";
-import { Box, Grid, Heading, Text } from "theme-ui";
+import { Box, Grid, Heading, Text, Link, NavLink, Image } from "theme-ui";
+import GitHubButton from "react-github-btn";
 
 import Main from "../layouts/Main";
+
+const repo = "https://github.com/sohrab-/pets";
+const gitHubButtons = [
+  { name: "Star", icon: "octicon-star", href: repo },
+  { name: "Watch", icon: "octicon-eye", href: `${repo}/subscription` },
+  { name: "Fork", icon: "octicon-repo-forked", href: `${repo}/fork` },
+  { name: "Issues", icon: "octicon-issue-opened", href: `${repo}/issues` },
+];
+const contributors = ["halfbakedsneed", "sohrab-", "tsukidust"];
 
 function About() {
   return (
     <Main>
-      <Box mb={20}>
+      <Box mb={5}>
         <Heading>About</Heading>
       </Box>
-      <Box mb={20}>
+      <Box mb={1}>
+        <Text>We're open source</Text>
+      </Box>
+      <Box mb={1}>
+        <NavLink href={repo}>{repo}</NavLink>
+      </Box>
+      <Box mb={5}>
+        {gitHubButtons.map((button) => (
+          <>
+            <GitHubButton
+              href={button.href}
+              data-icon={button.icon}
+              data-size="small"
+              aria-label={`${button.name} sohrab-/pets on GitHub`}
+            >
+              {button.name}
+            </GitHubButton>{" "}
+          </>
+        ))}
+      </Box>
+      <Box mb={3}>
         <Text sx={{ fontWeight: "bold" }}>Created by</Text>
       </Box>
-      <Grid columns={[1, null, 3]} mb={20}>
-        <Text>halfbakedsneed</Text>
-        <Text>sohrab-</Text>
-        <Text>tsukidust</Text>
+      <Grid columns={[1, 3, 3]} mb={6}>
+        {contributors.map((contributor) => (
+          <Box>
+            <NavLink
+              href={`https://github.com/${contributor}`}
+              sx={{ fontWeight: "normal" }}
+            >
+              <Image
+                src={`https://github.com/${contributor}.png`}
+                sx={{
+                  verticalAlign: "middle",
+                  borderRadius: "50%",
+                  maxWidth: "100px",
+                }}
+              />
+            </NavLink>
+          </Box>
+        ))}
       </Grid>
       <Box py={2}>
         <Text fontWeight="lighter" fontSize="smaller">
           Icons made by{" "}
-          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+          <Link href="https://www.flaticon.com/authors/freepik" title="Freepik">
             Freepik
-          </a>{" "}
+          </Link>{" "}
           from{" "}
-          <a href="https://www.flaticon.com/" title="Flaticon">
+          <Link href="https://www.flaticon.com/" title="Flaticon">
             www.flaticon.com
-          </a>
+          </Link>
         </Text>
       </Box>
     </Main>
